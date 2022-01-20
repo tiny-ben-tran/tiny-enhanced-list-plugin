7bb226c02672ad951aead169c86918e214059416
Ben Tran <ben.tran@tiny.cloud>
2022-01-20 11:27:28 +1030

    Moved some code to Actions.ts

--- src/main/ts/core/Actions.ts
+++ src/main/ts/core/Actions.ts
@@ -1,10 +1,39 @@
 
-import { Editor } from 'tinymce';
+import { Traverse, SugarElement, PredicateFilter } from '@ephox/sugar';
+import { Arr } from '@ephox/katamari';
 
-const applyUnorderList = function (editor: Editor): void {
-  editor.execCommand('InsertUnOrderedList', false, {});
+type ApplyingOption  = 'listAndParents' | 'listAndDecendants' | 'wholetree' | 'list';
+
+function isOLULNode(e: SugarElement<Node>): boolean {
+  return Arr.contains(['UL', 'OL'], e.dom.nodeName) === true;
+}
+
+function getListAndItemNodes(selectedEl: SugarElement<Element>, applyingOption: ApplyingOption): Node[] {
+  // TODO: Maybe need to handle where parent of LI isn't an OL or UL node?
+  const currenListNode = Traverse.parentNode(selectedEl).getOrDie('Can not find parent node');
+  // apply to the selected list by default
+  let listNodes: SugarElement<Node>[] = [currenListNode];
+  // Add ancestor or decedant UL and OL nodes
+  if (applyingOption === 'listAndParents') {
+    const ancestorNodes = PredicateFilter.ancestors(currenListNode, isOLULNode);
+    listNodes = listNodes.concat(ancestorNodes);
+  } else if (applyingOption === 'listAndDecendants') {
+    const descendantNodes = PredicateFilter.descendants(currenListNode, isOLULNode);
+    listNodes = listNodes.concat(descendantNodes);
+  } else if (applyingOption === 'wholetree') {
+    const ancestorNodes = PredicateFilter.ancestors(currenListNode, isOLULNode);
+    const descendantNodes = PredicateFilter.descendants(currenListNode, isOLULNode);
+    listNodes = listNodes.concat(ancestorNodes).concat(descendantNodes);
+  }
+
+  // get LI of each OL and UL
+  const liItems = Arr.flatten(Arr.map(listNodes, (e) => PredicateFilter.children(e, (i) => i.dom.nodeName === 'LI')));
+  listNodes = listNodes.concat(liItems);
+  return Arr.map(listNodes, (e) => e.dom);
 }
 
 export {
-  applyUnorderList
+  getListAndItemNodes,
+  isOLULNode,
+  ApplyingOption
 }
--- src/main/ts/ui/Dialog.ts
+++ src/main/ts/ui/Dialog.ts
@@ -1,41 +1,11 @@
 import { Editor } from 'tinymce';
-import { Traverse, SugarElement, PredicateFilter } from '@ephox/sugar';
 import { Arr } from '@ephox/katamari';
-type ApplyingOption  = 'listAndParents' | 'listAndDecendants' | 'wholetree' | 'list';
-
+import * as Actions from '../core/Actions';
+import { PredicateFilter, SugarElement } from '@ephox/sugar';
 interface DialogData {
   readonly listStyle: string;
-  readonly applyingOption: ApplyingOption;
+  readonly applyingOption: Actions.ApplyingOption;
   readonly paddingValue: string;
-}
-
-
-function isOLULNode(e: SugarElement<Element>) {
-  return Arr.contains(['UL', 'OL'], e.dom.nodeName) === true;
-}
-
-function getListAndItemNodes(selectedEl: SugarElement<Element>, applyingOption: string): Node[] {
-  // TODO: Maybe need to handle where parent of LI isn't an OL or UL node?
-  const currenListNode = Traverse.parentNode(selectedEl).getOrDie('Can not find parent node');
-  // apply to the selected list by default
-  let listNodes: SugarElement<Node>[] = [currenListNode];
-  // Add ancestor or decedant UL and OL nodes
-  if (applyingOption === 'listAndParents') {
-    const ancestorNodes = PredicateFilter.ancestors(currenListNode, isOLULNode);
-    listNodes = listNodes.concat(ancestorNodes);
-  } else if (applyingOption === 'listAndDecendants') {
-    const descendantNodes = PredicateFilter.descendants(currenListNode, isOLULNode);
-    listNodes = listNodes.concat(descendantNodes);
-  } else if (applyingOption === 'wholetree') {
-    const ancestorNodes = PredicateFilter.ancestors(currenListNode, isOLULNode);
-    const descendantNodes = PredicateFilter.descendants(currenListNode, isOLULNode);
-    listNodes = listNodes.concat(ancestorNodes).concat(descendantNodes);
-  }
-
-  // get LI of each OL and UL
-  const liItems = Arr.flatten(Arr.map(listNodes, (e) => PredicateFilter.children(e, (i) => i.dom.nodeName === 'LI')));
-  listNodes = listNodes.concat(liItems);
-  return Arr.map(listNodes, (e) => e.dom);
 }
 
 const register = (editor: Editor, selectedEl: Element): void => {
@@ -76,7 +46,7 @@ const register = (editor: Editor, selectedEl: Element): void => {
   ];
 
   // dropdown for nested list
-  if (PredicateFilter.ancestors(sugarEl, isOLULNode).length > 1) {
+  if (PredicateFilter.ancestors(sugarEl, Actions.isOLULNode).length > 1) {
     panelItems.push({
       type: 'bar',
       items: [
@@ -117,7 +87,7 @@ const register = (editor: Editor, selectedEl: Element): void => {
     ],
     onSubmit: function (dialogApi) {
       const {listStyle, paddingValue, applyingOption} = dialogApi.getData() as DialogData;
-      const nodes = getListAndItemNodes(sugarEl, applyingOption);
+      const nodes = Actions.getListAndItemNodes(sugarEl, applyingOption);
       editor.undoManager.transact(function() {
         Arr.each(nodes, (n) => {
           if (/OL|UL/.test(n.nodeName) === true) {
