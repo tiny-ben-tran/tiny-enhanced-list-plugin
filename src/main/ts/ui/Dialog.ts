c2cf8ee9ccce306f95603285c2003a30e01f324b
Ben Tran <ben.tran@tiny.cloud>
2022-01-20 11:11:38 +1030

    Clean up

--- src/main/ts/ui/Dialog.ts
+++ src/main/ts/ui/Dialog.ts
@@ -1,54 +1,41 @@
 import { Editor } from 'tinymce';
 import { Traverse, SugarElement, PredicateFilter } from '@ephox/sugar';
 import { Arr } from '@ephox/katamari';
+type ApplyingOption  = 'listAndParents' | 'listAndDecendants' | 'wholetree' | 'list';
 
 interface DialogData {
   readonly listStyle: string;
-  readonly applyingOption: string;
+  readonly applyingOption: ApplyingOption;
   readonly paddingValue: string;
 }
 
+
 function isOLULNode(e: SugarElement<Element>) {
-  return Arr.contains(["UL", "OL"], e.dom.nodeName) === true;
+  return Arr.contains(['UL', 'OL'], e.dom.nodeName) === true;
 }
 
 function getListAndItemNodes(selectedEl: SugarElement<Element>, applyingOption: string): Node[] {
   // TODO: Maybe need to handle where parent of LI isn't an OL or UL node?
-  const currenListNode = Traverse.parentNode(selectedEl).getOrDie("Can not find parent node");
-  let listNodes: SugarElement<Node>[] = [];
-  // Add ancestor or decedant UL and OL
-  if (applyingOption === "selectedListAndParents") {
+  const currenListNode = Traverse.parentNode(selectedEl).getOrDie('Can not find parent node');
+  // apply to the selected list by default
+  let listNodes: SugarElement<Node>[] = [currenListNode];
+  // Add ancestor or decedant UL and OL nodes
+  if (applyingOption === 'listAndParents') {
     const ancestorNodes = PredicateFilter.ancestors(currenListNode, isOLULNode);
     listNodes = listNodes.concat(ancestorNodes);
-  } else if (applyingOption === "selectedListAndDecendants") {
+  } else if (applyingOption === 'listAndDecendants') {
     const descendantNodes = PredicateFilter.descendants(currenListNode, isOLULNode);
     listNodes = listNodes.concat(descendantNodes);
-  } else if (applyingOption === "wholetree") {
+  } else if (applyingOption === 'wholetree') {
     const ancestorNodes = PredicateFilter.ancestors(currenListNode, isOLULNode);
     const descendantNodes = PredicateFilter.descendants(currenListNode, isOLULNode);
     listNodes = listNodes.concat(ancestorNodes).concat(descendantNodes);
   }
 
   // get LI of each OL and UL
-  const liItems = Arr.flatten(Arr.map(listNodes, (e) => PredicateFilter.children(e, (i) => i.dom.nodeName === "LI")));
+  const liItems = Arr.flatten(Arr.map(listNodes, (e) => PredicateFilter.children(e, (i) => i.dom.nodeName === 'LI')));
   listNodes = listNodes.concat(liItems);
   return Arr.map(listNodes, (e) => e.dom);
-}
-
-/**
- * Apply style to OL, UL and padding to LI nodes
- * @param nodes
- * @param listStyle Values of list-style-type
- * @param padding Apply to LI nodes
- */
-function applyStyleToNodes(editor: Editor, nodes: Node[], listStyle: string, padding: string) {
-  Arr.each(nodes, (n) => {
-    if (/OL|UL/.test(n.nodeName) === true) {
-      editor.dom.setStyle(n, 'list-style-type', listStyle);
-    } else if (n.nodeName === "LI" && /[0-9]+/.test(padding)) {
-      editor.dom.setStyle(n, 'padding-left', padding + "px");
-    }
-  });
 }
 
 const register = (editor: Editor, selectedEl: Element): void => {
@@ -102,10 +89,10 @@ const register = (editor: Editor, selectedEl: Element): void => {
               value: 'selectedList', text: 'Selected list'
             },
             {
-              value: 'selectedListAndParents', text: 'Selected list + all parent lists'
+              value: 'listAndParents', text: 'Selected list + all parent lists'
             },
             {
-              value: 'selectedListAndDecendants', text: 'Selected list + all children lists'
+              value: 'listAndDecendants', text: 'Selected list + all children lists'
             },
             {
               value: 'wholetree', text: 'All lists in the current tree'
@@ -132,7 +119,13 @@ const register = (editor: Editor, selectedEl: Element): void => {
       const {listStyle, paddingValue, applyingOption} = dialogApi.getData() as DialogData;
       const nodes = getListAndItemNodes(sugarEl, applyingOption);
       editor.undoManager.transact(function() {
-        applyStyleToNodes(editor, nodes, listStyle, paddingValue);
+        Arr.each(nodes, (n) => {
+          if (/OL|UL/.test(n.nodeName) === true) {
+            editor.dom.setStyle(n, 'list-style-type', listStyle);
+          } else if (n.nodeName === 'LI' && /[0-9]+/.test(paddingValue) === true) {
+            editor.dom.setStyle(n, 'padding-left', paddingValue + 'px');
+          }
+        });
       });
       dialogApi.close();
     },
